8a2ce8dd681971cfce86498983624b515eb295f0
yursds <yursds@outlook.com>
2024-06-17 09:36:01 +0000

    small changes urdf

--- src/softleg_description/launch/display_softleg.launch.py
+++ src/softleg_description/launch/display_softleg.launch.py
@@ -1,74 +1,77 @@
 # import imp
 import os
-from ament_index_python.packages import get_package_share_path
 
-from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import Command,LaunchConfiguration,PathJoinSubstitution
-from launch.conditions import IfCondition,UnlessCondition
+from launch                             import LaunchDescription
+from launch.actions                     import DeclareLaunchArgument
+from launch.substitutions               import Command, LaunchConfiguration, PathJoinSubstitution
+from launch.conditions                  import IfCondition, UnlessCondition
 
-from launch_ros.actions import Node
-from launch_ros.parameter_descriptions import ParameterValue
+from launch_ros.actions                 import Node
+from launch_ros.parameter_descriptions  import ParameterValue
+
+from ament_index_python.packages        import get_package_share_path
 
 def generate_launch_description():
+    
     URDF_FILE = 'softlegisaac.urdf'
 
     # path for build softleg urdf
     softleg_robot_path = get_package_share_path("softleg_description")
     softleg_robot_path = os.path.join(softleg_robot_path, "urdf", URDF_FILE)
-    # softleg_robot_path = os.path.join(softleg_robot_path, "urdf", "softleg.xacro") 
+    # softleg_robot_path = os.path.join(softleg_robot_path, "urdf", "softleg.xacro")
+    
     # path for rviz settings
     rviz_config_path = get_package_share_path("softleg_description")
     rviz_config_path = os.path.join(rviz_config_path, "rviz", "config.rviz")
 
     #declaration argument of launch
     use_gui = DeclareLaunchArgument(
-        name="use_gui",
-        default_value="true",
-        description="Value use to enable joint publisher with GUI")
+        name          = "use_gui",
+        default_value = "true",
+        description   = "Value use to enable joint publisher with GUI")
 
     softleg_model = DeclareLaunchArgument(
-        name="softleg",
-        default_value=str(softleg_robot_path)
+        name          = "softleg",
+        default_value = str(softleg_robot_path)
     )
 
     rviz_arg = DeclareLaunchArgument(
-        name="rviz_config",
-        default_value= str(rviz_config_path),
-        description="configuration of Rviz for plot"
+        name          = "rviz_config",
+        default_value = str(rviz_config_path),
+        description   = "configuration of Rviz for plot"
     )
 
     # use command to create a parameter with urdf of softleg by xacro file
     robot_description = ParameterValue(
-        Command(["xacro ",LaunchConfiguration("softleg")]),
-        value_type=str
+        Command(["xacro ", LaunchConfiguration("softleg")]),
+        value_type = str
     )
 
     # node declaration
     robot_state_pub = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        parameters=[{'robot_description': robot_description}]
+        package    = 'robot_state_publisher',
+        executable = 'robot_state_publisher',
+        parameters = [{'robot_description': robot_description}]
     )
 
     robot_joint_pub = Node(
-        package='joint_state_publisher',
-        executable='joint_state_publisher',
-        condition= UnlessCondition(LaunchConfiguration('use_gui')) 
+        package    = 'joint_state_publisher',
+        executable = 'joint_state_publisher',
+        condition  = UnlessCondition(LaunchConfiguration('use_gui'))
     )
 
     robot_joint_pub_gui = Node(
-        package='joint_state_publisher_gui',
-        executable='joint_state_publisher_gui',
-        condition= IfCondition(LaunchConfiguration('use_gui')) 
+        package    = 'joint_state_publisher_gui',
+        executable = 'joint_state_publisher_gui',
+        condition  = IfCondition(LaunchConfiguration('use_gui'))
     )
 
     rviz_node = Node(
-        package='rviz2',
-        executable='rviz2',
-        name='rviz',
-        output= 'screen',
-        arguments=['-d', rviz_config_path],
+        package    = 'rviz2',
+        executable = 'rviz2',
+        name       = 'rviz',
+        output     = 'screen',
+        arguments  = ['-d', rviz_config_path],
     )
 
     return LaunchDescription(

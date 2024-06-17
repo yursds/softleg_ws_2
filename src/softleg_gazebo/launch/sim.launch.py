1515c5f0b79bcdfcdf4278441757386a78f0d16c
yursds <yursds@outlook.com>
2024-06-17 13:32:09 +0000

    add comments

--- src/softleg_gazebo/launch/sim.launch.py
+++ src/softleg_gazebo/launch/sim.launch.py
@@ -5,66 +5,72 @@ from launch_ros.substitutions           import FindPackageShare
 
 
 from launch                             import LaunchDescription
-from launch.actions                     import IncludeLaunchDescription, RegisterEventHandler, DeclareLaunchArgument
+from launch.actions                     import IncludeLaunchDescription, RegisterEventHandler
 
-from launch.substitutions               import PathJoinSubstitution, LaunchConfiguration
+from launch.substitutions               import PathJoinSubstitution
 from launch.launch_description_sources  import PythonLaunchDescriptionSource
 from launch.event_handlers              import OnProcessExit
-from launch.conditions                  import IfCondition, UnlessCondition
 
 from ament_index_python.packages        import get_package_share_directory
 
 def generate_launch_description():
     
-    gazebo = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([PathJoinSubstitution([FindPackageShare("gazebo_ros"), "launch", "gazebo.launch.py"])]),
-        launch_arguments={"pause": "false", "verbose": "true"}.items(),
-    )
-    
+    # define path and contend of .urdf file
     softleg_description_path = os.path.join(
         get_package_share_directory('softleg_description'),
         'urdf',
-        'softlegisaac.urdf'
+        'softlegisaac.urdf')
+    softleg_description_content = open(softleg_description_path).read()
+    robot_description = {"robot_description": softleg_description_content}
+    
+    # launch gazebo.launch.py
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([PathJoinSubstitution([FindPackageShare("gazebo_ros"), "launch", "gazebo.launch.py"])]),
+        launch_arguments={"pause": "true", "verbose": "true"}.items(),
     )
     
-    softleg_description_content = open(softleg_description_path).read()
+    # ========================================= NODE ============================================= #
     
-    robot_description = {"robot_description": softleg_description_content}
-
-    node_robot_state_publisher = Node(
+    # robot_state_publisher: 
+    # read from topic /joint_states e pub the configuration using TF transforms.
+    robot_state_pub = Node(
         package    = "robot_state_publisher",
         executable = "robot_state_publisher",
         output     = "screen",
         parameters = [robot_description],
     )
 
+    # entity in gazebo simulator (with name softleg): 
+    # input -> URDF (from gazebo, [robot_description]) and initial condition.
     spawn_entity = Node(
         package    = "gazebo_ros",
         executable = "spawn_entity.py",
         arguments  = [
             "-topic", "robot_description", "-entity", "softleg",
-            "-x", "0.0", "-y", "0.0", "-z", "0.0", ],
+            "-x", "0.0", "-y", "0.0", "-z", "0.3", ],
         output     = "screen",
     )
 
+    # the broadcaster reads all state interfaces and pub them on /joint_states and /dynamic_joint_states.
+    # it is not a real controller.
     joint_state_broadcaster_spawner = Node(
         package    = "controller_manager",
         executable = "spawner",
         arguments  = ["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
     )
 
-    PD_jnt_control = Node(
+    # PD controller
+    PD_control = Node(
         package    = "controller_manager",
         executable = "spawner",
         arguments  = ["PD_control", "--controller-manager", "/controller_manager"],
     )
-
     
     return LaunchDescription([
         
         SetParameter(name='use_sim_time', value=True),
         gazebo,
-        node_robot_state_publisher,
+        robot_state_pub,
         spawn_entity,
         RegisterEventHandler(
             event_handler=OnProcessExit(
@@ -75,7 +81,7 @@ def generate_launch_description():
         RegisterEventHandler(
             event_handler=OnProcessExit(
                 target_action = spawn_entity,
-                on_exit       = [PD_jnt_control],
+                on_exit       = [PD_control],
             )
         ),
     ])
